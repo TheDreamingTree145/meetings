2752e93dd8b62686ef6e510ffa02ebb943fec998
TheDreamingTree145 <dandersonsoftware@outlook.com>
2018-08-17 20:07:25 -0400

    removed scope method

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,7 +1,4 @@
 class Meeting < ApplicationRecord
   belongs_to :student
   belongs_to :project, optional: :true
-
-  scope :project_meetings, -> (project_id) { where("project_id = ?", project_id) }
-
 end
